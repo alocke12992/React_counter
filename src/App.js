0700a6e2051ff83b0ed9a137cf81bef0b1c2b412
Andrew Locke <alocke12992@gmail.com>
2018-03-07 10:23:33 -0700

    Adds counter

--- src/App.js
+++ src/App.js
@@ -2,17 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: 0 } // STATE IS ALWAYS AN OBJECT 
+    this.inc = this.inc.bind(this)
+    this.dec = this.dec.bind(this)
+  }
+
+  inc() {
+    this.setState({ value: this.state.value + 1})
+  }
+
+  dec() {
+    const { value } = this.state 
+    if ( value > 0 ) 
+    this.setState({ value: this.state.value - 1 })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Andrew's React App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  
+        <div>
+          <p>{this.state.value}</p> 
+          <button onClick={this.inc}>+</button>
+          <button onClick={this.dec}>-</button> 
+        </div>
       </div>
     );
   }
